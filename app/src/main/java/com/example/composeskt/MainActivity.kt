418059470b720069cfc17707f1ff87f70ae49215
Prafful <praffuljha1234@gmail.com>
2024-09-06 16:18:52 +0530

    Lasy Column Added

--- app/src/main/java/com/example/composeskt/MainActivity.kt
+++ app/src/main/java/com/example/composeskt/MainActivity.kt
@@ -1,73 +1,65 @@
 package com.example.composeskt
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.foundation.layout.fillMaxWidth
+
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+
+import androidx.compose.foundation.lazy.LazyColumn
+
 import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composeskt.ui.theme.ComposesKtTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Counter()
+            MainScreen()
         }
     }
 }
 
 @Preview
 @Composable
-fun Counter(){
-    var backgroundColor by remember { mutableStateOf(Color.Gray) }
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(backgroundColor).padding(vertical = 32.dp), contentAlignment = Alignment.BottomCenter){
-        Row( horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalAlignment = Alignment.CenterVertically) {
-            Button(onClick = {backgroundColor = Color.Red }) {
-                Text("Red")
-            }
-            Button(onClick = {backgroundColor = Color.Blue }) {
-                Text("BLue")
-            }
-            Button(onClick = {backgroundColor = Color.Yellow }) {
-                Text("Yellow")
-            }
-            Button(onClick = {backgroundColor = Color.Green }) {
-                Text("Green")
+fun MainScreen(){
+    val words = arrayOf("JAVA","PYTHON","KOTLIN","PHP","C++","C","NEXT.JS","REACT.JS","LUA","JIRA","MONGO.DB")
+    val color = arrayOf(Color.Gray,Color.Cyan,Color.Green,Color.Red,Color.Gray,Color.Magenta,Color.Yellow,Color.LightGray,Color.Blue,Color.White,Color.Transparent)
+    LazyColumn(Modifier.background(color = Color.Black), horizontalAlignment = Alignment.CenterHorizontally) {
+        items(words.size){ index ->
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .background(color = color[index]),  // Card takes up the full width
+                elevation = CardDefaults.cardElevation(8.dp)
+            ) {
+                Text(
+                    text = words[index],
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .align(Alignment.CenterHorizontally),
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
-
 }
 
 
